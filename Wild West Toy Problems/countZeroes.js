855accdf5837f8c6ad903a78fdd991969a90c922
Leslie Ngo <ngo.leslie@gmail.com>
2021-10-11 09:21:12 -0700

    added tests

--- Wild West Toy Problems/countZeroes.js
+++ Wild West Toy Problems/countZeroes.js
@@ -11,3 +11,14 @@ function countZeroes(arr) {
   }
   return arr.length;
 }
+
+let a = [1,1,1,1,0,0,0,0,0,0,0,0,0];
+let b = [1,1,1,1,1,1,1,0,0,0,0,0,0];
+let c = [1,1,1,1,1,1,1,1,1,1,1,1,1];
+let d = [0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+console.log('Array Length: ', a.length)
+console.log(countZeroes(a));
+console.log(countZeroes(b));
+console.log(countZeroes(c));
+console.log(countZeroes(d));
